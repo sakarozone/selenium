0eb5ff87747cc9078fbf6fed2b6a8ce1f0337045
sakarozone <sakar@ozone.one>
2023-03-18 20:06:09 +0530

    env

--- script.py
+++ script.py
@@ -3,13 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 options = Options()
-print('asdf')
-print(sys.argv[1])
-print(sys.argv[2])
 options.headless = True
 driver = webdriver.Firefox(options=options)
-driver.get("sys.argv[0]")
-element = driver.find_element(By.ID, "sys.argv[1]")
+driver.get("sys.argv[1]")
+element = driver.find_element(By.ID, "sys.argv[2]")
 assert element.is_displayed()
 if element.is_displayed():
     print("The button was found!")
