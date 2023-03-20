05ec9e90d38a0187096e59e76f2956219f97ed80
sakarozone <sakar@ozone.one>
2023-03-20 15:55:16 +0530

    test

--- script.py
+++ script.py
@@ -4,25 +4,24 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 options = Options()
-arr = sys.argv[1]
-print(type(arr))
-arr.replace('[\'', '')
-arr.replace('\']', '')
-# path=arr[0]
-print(arr)
+str = sys.argv[1]
+str.replace('[\'', '')
+str.replace('\']', '')
+print(str)
+arr= str.split(" ")
+path=arr[0]
+options.headless = True
+driver = webdriver.Firefox(options=options)
+driver.get(path)
+length=len(arr)
+print('lengthis',length)
+for i in range(1,length):
+    try:
+        element = driver.find_element(By.ID, arr[i])
+        assert element.is_displayed()
+        if element.is_displayed():
+            print("The element ",arr[i], " was found!")
+    except NoSuchElementException:
+        print("The element ",arr[i]," was not found!")
 
-# options.headless = True
-# driver = webdriver.Firefox(options=options)
-# driver.get(path)
-# length=len(arr)
-# print('lengthis',length)
-# for i in range(1,length):
-#     try:
-#         element = driver.find_element(By.ID, arr[i])
-#         assert element.is_displayed()
-#         if element.is_displayed():
-#             print("The element ",arr[i], " was found!")
-#     except NoSuchElementException:
-#         print("The element ",arr[i]," was not found!")
-
-# driver.quit()
+driver.quit()
