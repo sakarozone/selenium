554cdd728f201930734a37a9e8426cd35ca045d3
sakarozone <sakar@ozone.one>
2023-03-20 14:02:17 +0530

    changes for multi args

--- script.py
+++ script.py
@@ -4,16 +4,19 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 options = Options()
-path=sys.argv[1]
-id=sys.argv[2]
+arr = sys.argv[1].split(',')
+path=arr[0]
 options.headless = True
 driver = webdriver.Firefox(options=options)
 driver.get(path)
-try:
-    element = driver.find_element(By.ID, id)
-    assert element.is_displayed()
-    if element.is_displayed():
-        print("The element was found!")
-except NoSuchElementException:
-    print("The element was not found!")
+length=len(arr)
+for i in range(1,length+1):
+    try:
+        element = driver.find_element(By.ID, arr[i])
+        assert element.is_displayed()
+        if element.is_displayed():
+            print("The element ",arr[i], " was found!")
+    except NoSuchElementException:
+        print("The element ",arr[i]," was not found!")
+        
 driver.quit()
