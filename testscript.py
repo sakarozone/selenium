a847789c922dc2bcb9fe41fb9a6ac778e64d64df
sakarozone <sakar@ozone.one>
2023-03-20 22:47:19 +0530

    testscriptmod

--- testscript.py
+++ testscript.py
@@ -1,10 +1,9 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(options=chrome_options)
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(options=options)
 driver.get("http://www.seleniumframework.com/Practiceform/")
 element = driver.find_element(By.ID, "button1")
 assert element.is_displayed()
